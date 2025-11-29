e0aa13bb6788c54dc5ddf498da23c02632f9b4e5
Yuzu <128008000+ailu2533@users.noreply.github.com>
2025-11-29 20:27:12 +0800

    Clean up FFmpegKitSPM.swift by removing comments
    Removed comments and documentation from FFmpegKitSPM.swift.

--- Sources/FFmpegKitSPM.swift
+++ Sources/FFmpegKitSPM.swift
@@ -1,21 +1,15 @@
-// FFmpegKitSPM - Swift Package wrapper for FFmpeg libraries
-// This file provides access to the FFmpeg binary frameworks
+// FFmpegKitSPM.swift
+@_exported import libavcodec
+@_exported import libavformat
+@_exported import libavutil
+@_exported import libswresample
+@_exported import libswscale
+@_exported import libavfilter
+@_exported import libavdevice
 
 import Foundation
 
-/// A wrapper module that provides access to FFmpeg libraries through Swift Package Manager.
-///
-/// This module includes the following FFmpeg libraries:
-/// - libavcodec: Encoding/decoding library
-/// - libavdevice: Special devices handling
-/// - libavfilter: Graph-based frame editing library
-/// - libavformat: I/O and muxing/demuxing library
-/// - libavutil: Common utility library
-/// - libswresample: Audio resampling library
-/// - libswscale: Color conversion and scaling library
 public struct FFmpegKitSPM {
-    /// The version of this package wrapper
     public static let version = "1.0.0"
-    
     private init() {}
 }
